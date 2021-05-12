b58d62d9cfebb0dc2ee1b6338c252332702dac00
Chintan Vora <c.vora@somaiya.edu>
2021-05-12 18:59:21 +0530

    Chintan's Commit

---
+++ app.py
@@ -0,0 +1,77 @@
+
+from flask import Flask, render_template, request, send_from_directory
+import utils
+import train_models as tm
+import os
+import pandas as pd
+
+app = Flask(__name__)
+
+def perform_training(stock_name, df, models_list):
+    all_colors = {
+                  'linear_regression': '#CC2A1E',
+                  'random_forests': '#8F0099',
+                  'KNN': '#CCAB43',
+                  'DT': '#85CC43',
+                  'LSTM_model': '#CC7674'}
+
+    print(df.head())
+    dates, prices, ml_models_outputs, prediction_date, test_price = tm.train_predict_plot(stock_name, df, models_list)
+    origdates = dates
+    if len(dates) > 20:
+        dates = dates[-20:]
+        prices = prices[-20:]
+
+    all_data = []
+    all_data.append((prices, 'false', 'Data', '#000000'))
+    for model_output in ml_models_outputs:
+        if len(origdates) > 20:
+            all_data.append(
+                (((ml_models_outputs[model_output])[0])[-20:], "true", model_output, all_colors[model_output]))
+        else:
+            all_data.append(
+                (((ml_models_outputs[model_output])[0]), "true", model_output, all_colors[model_output]))
+
+    all_prediction_data = []
+    all_test_evaluations = []
+    all_prediction_data.append(("Original", test_price))
+    for model_output in ml_models_outputs:
+        all_prediction_data.append((model_output, (ml_models_outputs[model_output])[1]))
+        all_test_evaluations.append((model_output, (ml_models_outputs[model_output])[2]))
+
+    return all_prediction_data, all_prediction_data, prediction_date, dates, all_data, all_data, all_test_evaluations
+
+all_files = utils.read_all_stock_files('stocks_dataset')
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def landing_function():
+    stock_files = list(all_files.keys())
+
+    return render_template('index.html',show_results="false", stocklen=len(stock_files), stock_files=stock_files, len2=len([]),
+                           all_prediction_data=[],
+                           prediction_date="", dates=[], all_data=[], len=len([]))
+
+@app.route('/process', methods=['POST'])
+def process():
+
+    stock_file_name = request.form['stockfile']
+    ml_algoritms = request.form.getlist('mlalgos')
+
+    # all_files = utils.read_all_stock_files('stocks_dataset')
+    df = all_files[str(stock_file_name)]
+    # df = pd.read_csv('GOOG_30_days.csv')
+    all_prediction_data, all_prediction_data, prediction_date, dates, all_data, all_data, all_test_evaluations = perform_training(str(stock_file_name), df, ml_algoritms)
+    stock_files = list(all_files.keys())
+
+    return render_template('index.html',all_test_evaluations=all_test_evaluations, show_results="true", stocklen=len(stock_files), stock_files=stock_files,
+                           len2=len(all_prediction_data),
+                           all_prediction_data=all_prediction_data,
+                           prediction_date=prediction_date, dates=dates, all_data=all_data, len=len(all_data))
+
+# main driver function
+if __name__ == '__main__':
+    # run() method of Flask class runs the application
+    app.run()
